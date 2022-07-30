79518ce42330c613148d95785e85812a9ac071ae
Nathália Marques <74634442+nathaalua@users.noreply.github.com>
2022-07-29 22:01:15 -0300

    Update psicologosRotas.js

--- src/routes/psicologosRotas.js
+++ src/routes/psicologosRotas.js
@@ -6,9 +6,11 @@ const controller = require('../controllers/psicologosController')
 
 const router = express.Router()
 
-router.get('/biblioteca',      controller.findAllPsicologos)
+router.get('/biblioteca', controller.findAllPsicologos)
 router.get("/payment", controller.findPsicologosByPayment)
 router.get('/:id', controller.findById)
 router.post("/create", controller.createPsicologos)
+router.patch("/update/:id", controller.updatePsicologo)
+router.delete("/delete/:id", controller.deletePsicologo)
 
 module.exports = router
